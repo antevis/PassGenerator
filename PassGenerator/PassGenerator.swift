4fc75ccb2b592759dceecd81b6fc791cb034764e
Ivan Kazakov <polyconnect@me.com>
2016-07-11 16:48:23 +0300

    Seemingly full protocol hierarchy, before some meaningful refactoring

--- PassGenerator/PassGenerator.swift
+++ PassGenerator/PassGenerator.swift
@@ -25,6 +25,13 @@ enum DiscountSubject {
 	case merchandise
 }
 
+enum ManagementTier {
+	
+	case shift
+	case general
+	case senior
+}
+
 
 //MARK: structs
 
@@ -50,36 +57,162 @@ struct EntrantRules {
 	
 }
 
+struct PersonFullName {
+	
+	let firstName: String
+	let lastName: String
+}
+
+struct Address {
+	
+	let streetAddress: String
+	let city: String
+	let state: String
+	let zip: String
+}
 
 
 //MARK: protocols
 
-protocol EntrantName {
+//Most protocols below deliberately define a single value, for absolute resшlience in constructing objects
+
+//Most entrants conform to this
+protocol DiscountClaimant {
 	
-	var firstName: String { get }
-	var lastName: String { get }
+	var discounts: [DiscountParams] { get }
 }
 
-protocol EntrantAddress {
+//Hourly Employee, Manager, Season Pass Guest, Senior Guest, ContractEmployee, Vendor
+protocol FullNameProvider {
 	
-	var streetAddress: String { get }
-	var city: String { get }
-	var state: String { get }
-	var zip: String { get }
+	var fullName: PersonFullName { get }
+}
+
+//Hourly Employee, Manager, Contract Employee
+protocol SSNProvider {
+	
+	var ssn: String { get }
+}
+
+//Hourly Employee, Manager, Season Pass Guest, Contract Employee
+protocol AddressProvider {
+	
+	var address: Address { get }
 	
 }
 
-protocol Birthday {
+//Free Child Guest, Hourly Employee, Manager, Season Pass Guest, Senior Guest, Contract Employee, Vendor
+protocol BirthdayProvider {
 	
 	var birthDate: NSDate { get }
 }
 
-protocol VisitDate {
+//Vendor, For Part 2
+//protocol VisitDateDependant {
+//	
+//	var visitDate: NSDate { get }
+//}
+
+//Manager
+protocol ManagementTierProvider {
 	
-	var visitDate: NSDate { get }
+	var tier: ManagementTier { get }
 }
 
+//Joins those who have to provide both: Employees, Managers
+protocol PersonalDataProvider: FullNameProvider, SSNProvider {
+	
+}
+
+//defines Employee properties
+protocol Employee: Entrant, FullNameProvider, AddressProvider, BirthdayProvider, DiscountClaimant {
+	
+
+}
+
+//extends employee properties with Management Tier
+protocol Manager: Employee, ManagementTierProvider {
+	
+}
+
+//Describes any type of entrant.
 protocol Entrant {
+	
+	var accessibleAreas: [Area] { get }
+	var accessRules: RideAccess { get }
 	
 	func swipe() -> EntrantRules
 }
+
+//MARK: Entrant classes
+
+class ClassicGuest: Entrant {
+	
+	let accessibleAreas: [Area] = [.amusement]
+	let accessRules: RideAccess = RideAccess(unlimitedAccess: true, skipLines: false)
+	
+	func swipe() -> EntrantRules {
+		return EntrantRules(areaAccess: accessibleAreas, rideAccess: accessRules, discountAccess: nil)
+	}
+}
+
+class VipGuest: Entrant, DiscountClaimant {
+	
+	let accessibleAreas: [Area] = [.amusement]
+	let accessRules: RideAccess = RideAccess(unlimitedAccess: true, skipLines: true)
+	
+	let discounts: [DiscountParams] = [
+		
+		DiscountParams(subject: .food, discountValue: 10),
+		DiscountParams(subject: .merchandise, discountValue: 20)
+	]
+	
+	func swipe() -> EntrantRules {
+		
+		return EntrantRules(areaAccess: accessibleAreas, rideAccess: accessRules, discountAccess: discounts)
+	}
+}
+
+class FreeChildGuest: ClassicGuest, BirthdayProvider {
+	
+	let birthDate: NSDate
+	
+	init(birthDate: NSDate) {
+		
+		self.birthDate = birthDate
+	}
+}
+
+class HourlyEmployeeCatering: Employee {
+	
+	let accessibleAreas: [Area] = [.amusement, .kitchen]
+	let accessRules: RideAccess = RideAccess(unlimitedAccess: true, skipLines: false)
+	
+	func swipe() -> EntrantRules {
+		return EntrantRules(areaAccess: accessibleAreas, rideAccess: accessRules, discountAccess: nil)
+	}
+	
+	let discounts: [DiscountParams] = [
+		
+		DiscountParams(subject: .food, discountValue: 15),
+		DiscountParams(subject: .merchandise, discountValue: 25)
+	]
+	
+	let birthDate: NSDate
+	
+	let fullName: PersonFullName
+	
+	let ssn: String
+	
+	let address: Address
+	
+	init(birthDate: NSDate, fullName: PersonFullName, socSecNumber: String, address: Address) {
+		
+		self.birthDate = birthDate
+		self.fullName = fullName
+		self.ssn = socSecNumber
+		self.address = address
+	}
+}
+
+
